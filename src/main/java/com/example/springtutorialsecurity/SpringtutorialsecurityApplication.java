913a346fffda3c42adda91eeb340dc837c0b1076
davidbcnguyen <101545438+davidbcnguyen@users.noreply.github.com>
2022-05-08 14:28:56 -0700

    First commit

---
+++ src/main/java/com/example/springtutorialsecurity/SpringtutorialsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtutorialsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtutorialsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtutorialsecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springtutorialsecurity/SpringtutorialsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtutorialsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtutorialsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
